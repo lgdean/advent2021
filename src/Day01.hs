d4726d84853b409e19b3a600c1109f19cea9d945
L Dean <lgdean@users.noreply.github.com>
2021-11-30 21:34:46 -0800

    Day 1 Part 2, plus excess line of code

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,6 +1,8 @@
 module Day01
     ( countIncreases,
-      doPart1
+      countTripleIncreases,
+      doPart1,
+      doPart2
     ) where
 
 import Debug.Trace (trace)
@@ -13,3 +15,15 @@ doPart1 :: [Char] -> Int
 doPart1 input =
   let inputInts = map read $ lines input
   in countIncreases inputInts
+
+countTripleIncreases :: [Int] -> Int
+countTripleIncreases input =
+  let triples = zip3 input (tail input) (tail $ tail input)
+      sums = zipWith (+) input (tail input)
+      actualSums = zipWith (+) sums (tail $ tail input)
+  in countIncreases actualSums
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  let inputInts = map read $ lines input
+  in countTripleIncreases inputInts
--- test/Day01Spec.hs
+++ test/Day01Spec.hs
@@ -23,3 +23,20 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "input"
       doPart1 input `shouldBe` 1184
+
+  describe "Part 2" $ do
+    it "can handle given example" $ do
+      countTripleIncreases [199,
+                      200,
+                      208,
+                      210,
+                      200,
+                      207,
+                      240,
+                      269,
+                      260,
+                      263] `shouldBe` 5
+
+    it "can solve Part 2" $ do
+      input <- readFile "input"
+      doPart2 input `shouldBe` 1158
