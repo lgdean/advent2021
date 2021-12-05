56b1306fca1751098b9a3856eae951e797d5a9ea
L Dean <lgdean@users.noreply.github.com>
2021-12-04 21:57:05 -0800

    day 5 part 2 replacing part 1

--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -7,19 +7,11 @@ import Day05
 
 spec :: Spec
 spec = do
-  describe "Part 1" $ do
+  describe "Part 2" $ do
     it "can handle given example" $ do
       input <- readFile "day05-example-input"
-      doPart1 input `shouldBe` 5
+      doPart1 input `shouldBe` 12
 
-    it "can solve Part 1" $ do
+    it "can solve Part 2" $ do
       input <- readFile "day05-input"
-      doPart1 input `shouldBe` 6687
-
---  describe "Part 2" $ do
---    it "can handle given example" $ do
---      pending
---
---    it "can solve Part 2" $ do
---      input <- readFile "day05-input"
---      pending
+      doPart1 input `shouldBe` 19851
