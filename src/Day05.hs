a57d0d0a7d777437b7d12b1c48369c75f3b49d00
L Dean <lgdean@users.noreply.github.com>
2021-12-04 22:04:05 -0800

    cleanup: remove unused

--- src/Day05.hs
+++ src/Day05.hs
@@ -13,16 +13,13 @@ import Debug.Trace (trace)
 type Line = ((Int, Int), (Int, Int))
 
 allPointsInLine :: Line -> [(Int, Int)]
-allPointsInLine line@((x1,y1),(x2,y2))
+allPointsInLine ((x1,y1),(x2,y2))
   | x1 == x2 = zip (repeat x1) [(min y1 y2) .. (max y1 y2)]
   | y1 == y2 = zip [(min x1 x2) .. (max x1 x2)] (repeat y1)
   | x1 < x2 && y1 < y2 = zip [x1..x2] [y1..y2]
   | x1 < x2  = zip [x1..x2] $ reverse [y2..y1]
   | y1 < y2 = zip (reverse [x2..x1]) [y1..y2]
   | otherwise = zip (reverse [x2..x1]) (reverse [y2..y1])
-
-isHorizontalOrVertical :: Line -> Bool
-isHorizontalOrVertical ((x1,y1),(x2,y2)) = x1==x2 || y1==y2
 
 parseLine :: String -> Line
 parseLine str =
