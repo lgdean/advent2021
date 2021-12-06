57e7de9b2aa7d9249699d8c44ab3b7460ff6a8ab
L Dean <lgdean@users.noreply.github.com>
2021-12-05 21:45:49 -0800

    yup, needed fancier fish-tracking method in part 2

--- src/Day06.hs
+++ src/Day06.hs
@@ -5,20 +5,31 @@ module Day06
       doPart1
     ) where
 
+import Data.Maybe (fromMaybe)
 import Data.List (group, sort)
-import Data.List.Split (splitOn)
 
 import Debug.Trace (trace)
 
 
 howManyAfter :: Int -> [Int] -> Int
 howManyAfter gens fish =
-  length (iterate (concatMap spawnDay) fish !! gens)
+  let initCount = map (\xs -> (head xs, length xs)) $ group $ sort fish
+  in countFish (iterate fancySpawn initCount !! gens)
 
--- for now do just a list, may need to graduate to fancier structure
-spawnDay :: Int -> [Int]
-spawnDay 0 = [6,8]
-spawnDay n = [n-1]
+countFish :: [(Int, Int)] -> Int
+countFish fishCounts = sum $ map snd fishCounts
+
+fancySpawn :: [(Int, Int)] -> [(Int, Int)]
+fancySpawn fishCounts =
+  let eights = lookupFish 0 fishCounts
+      sixes = lookupFish 0 fishCounts + lookupFish 7 fishCounts
+      sevens = lookupFish 8 fishCounts
+  in (6, sixes) : (7, sevens) : (8, eights)
+   : [(n, lookupFish (n+1) fishCounts) | n <- [0..5]]
+
+lookupFish :: Int -> [(Int, Int)] -> Int
+lookupFish fishKind fishCounts =
+  fromMaybe 0 (lookup fishKind fishCounts)
 
 doPart1 :: [Char] -> Int
 doPart1 _ = 0
