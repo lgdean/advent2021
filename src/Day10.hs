6466b8bc63df9ad6c2502422afa3238ef5acd1b2
L Dean <lgdean@users.noreply.github.com>
2021-12-09 21:43:57 -0800

    day 10, part 2

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,10 +1,42 @@
 module Day10
     (
---      doPart2,
+      doPart2,
       doPart1
     ) where
 
+import Data.List (sort)
+
 import Debug.Trace (trace)
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  let relevantLines = filter (not . isCorrupted) (lines input)
+      scores = map (part2ScoreLine . expectedCompletion []) relevantLines
+  in sort scores !! (length scores `div` 2)
+
+part2ScoreLine :: String -> Int
+part2ScoreLine str = foldl (\acc n -> acc*5+n) 0 (map part2Score str)
+
+isCorrupted :: String -> Bool
+isCorrupted line = scoreLine line > 0
+
+expectedCompletion :: [Char] -> [Char] -> [Char]
+expectedCompletion stack [] = stack
+expectedCompletion [] (')':_) = error "extra closer"
+expectedCompletion [] (']':_) = error "extra closer"
+expectedCompletion [] ('}':_) = error "extra closer"
+expectedCompletion [] ('>':_) = error "extra closer"
+expectedCompletion stack (x:xs)
+  | x `elem` ['(', '[', '{', '<'] = expectedCompletion (closerFor x : stack) xs
+  | x `elem` [')', ']', '}', '>'] && x == head stack = expectedCompletion (tail stack) xs
+  | x `elem` [')', ']', '}', '>'] && x /= head stack = error "corrupted line"
+
+part2Score :: Char -> Int
+part2Score ')' = 1
+part2Score ']' = 2
+part2Score '}' = 3
+part2Score '>' = 4
+part2Score x = error ("cannot score unexpected char " ++ [x])
 
 doPart1 :: [Char] -> Int
 doPart1 input =
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -15,3 +15,12 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "day10-input"
       doPart1 input `shouldBe` 319329
+
+  describe "Part 2" $ do
+    it "can handle given example" $ do
+      input <- readFile "day10-example-input"
+      doPart2 input `shouldBe` 288957
+
+    it "can solve Part 2" $ do
+      input <- readFile "day10-input"
+      doPart2 input `shouldBe` 3515583998
