c9d2bf78ccac07979cb47dab7b99b59e41c2e2f3
L Dean <lgdean@users.noreply.github.com>
2021-12-17 22:04:13 -0800

    day 18 parsing and magnitude

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,16 +1,43 @@
 module Day18
     (
 --      doPart2,
+      parseNum,
+      magnitude,
       doPart1
     ) where
 
+import Data.Char (digitToInt, intToDigit)
 
 import Debug.Trace (trace)
 
 import Lib
 
-data SnailfishNum = Regular Int | Pair SnailfishNum SnailfishNum
+data SnailfishNum = Regular Int | Pair SnailfishNum SnailfishNum deriving Show
 
 doPart1 :: [Char] -> Int
 doPart1 input =
   0
+
+magnitude :: SnailfishNum -> Int
+magnitude (Regular x) = x
+magnitude (Pair x y) = 3 * magnitude x + 2 * magnitude y
+
+-- this is where it would have been nice to learn Parsec
+parseNum :: String -> SnailfishNum
+parseNum input =
+  case parseInner input of
+    (result, [])  -> result
+    (_, leftover) -> error ("oops unexpected remaining string: " ++ leftover)
+
+parseInner :: String -> (SnailfishNum, String)
+parseInner ('[':content) =
+  let (firstResult,more) = parseInner content
+      (comma:second) = more
+      (secondResult,end) = parseInner second
+      (closer:remainder) = end
+  in case (comma, closer) of
+    (',', ']') -> (Pair firstResult secondResult, remainder)
+    _          -> error "mistake in parsing"
+parseInner (x:rest) =
+  (Regular (digitToInt x), rest)
+parseInner "" = error "parseInner on empty string, no good"
--- test/Day18Spec.hs
+++ test/Day18Spec.hs
@@ -7,6 +7,14 @@ import Day18
 
 spec :: Spec
 spec = do
+  describe "magnitude and parsing" $ do
+    it "can handle simple example" $ do
+      magnitude (parseNum "[9,1]") `shouldBe` 29
+    it "can handle first recursive example" $ do
+      magnitude (parseNum "[[9,1],[1,9]]") `shouldBe` 129
+    it "can handle a complext example" $ do
+      magnitude (parseNum "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]") `shouldBe` 3488
+
   describe "Part 1" $ do
     it "can handle given example" $ do
       input <- readFile "day18-example-input"
