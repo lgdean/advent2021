63995e268455b99cbb47c74d33d7dd1cff6769e1
L Dean <lgdean@users.noreply.github.com>
2021-12-19 17:42:48 -0800

    day 19: find overlap between a pair of beacons

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,10 +1,14 @@
 module Day19
     (
 --      doPart2,
+      findOverlapParsing,
+      rotationsOld,
       doPart1
     ) where
 
+import Data.List (intersect, maximumBy)
 import Data.List.Split (splitOn)
+import Data.Ord (comparing)
 
 import Debug.Trace (trace)
 
@@ -20,3 +24,50 @@ parseBeacon line =
   case splitOn "," line of
     [x,y,z] -> (read x, read y, read z)
     _       -> error $ "cannot parse: " ++ line
+
+findOverlap :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> ([(Int, Int, Int)], (Int, Int, Int))
+findOverlap ref other =
+  let rotatedOther = map (`map` other) rotations
+      overlaps = map (maxOverlap ref) rotatedOther
+  in maximumBy (comparing (length . fst)) overlaps
+
+maxOverlap :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> ([(Int, Int, Int)], (Int, Int, Int))
+maxOverlap ref other =
+  let diffsFromRef = concat [map (`distance3d` r) other | r <- ref] :: [(Int, Int, Int)]
+      overlaps = map (\d -> (overlapUsing d ref other, d)) diffsFromRef
+      mostInCommon = maximumBy (comparing (length . fst)) overlaps
+  in mostInCommon
+
+overlapUsing :: (Int, Int, Int) -> [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int, Int)]
+overlapUsing diff refs others =
+  let relativeOthers = map (`distance3d` diff) others
+  in intersect refs relativeOthers
+
+-- really a difference not a distance
+distance3d (a,b,c) (x,y,z) = (a-x, b-y, c-z)
+
+rotations :: [(Int, Int, Int) -> (Int, Int, Int)]
+rotations =
+  -- first treating z as a given "facing direction" and doing a cartwheel
+  let rotateWithSameFacing = [id, \(x,y,z) -> (-y,x,z), \(x,y,z) -> (-x,-y,z), \(x,y,z) -> (y,-x,z)]
+      -- then changing the facing direction: look [same], behind, up, down, left, right
+      turnAroundOrNot = [id, \(x,y,z) -> (-x,y,-z)]
+      lookSameUpOrRight = [id, \(x,y,z) -> (x,z,-y), \(x,y,z) -> (-z,y,x)]
+  in [r . dir . axis | r <- rotateWithSameFacing, dir <- turnAroundOrNot, axis <- lookSameUpOrRight]
+
+-- handy for testing
+rotationsOld :: (Int, Int, Int) -> [(Int, Int, Int)]
+rotationsOld p =
+  -- first treating z as a given "facing direction" and doing a cartwheel
+  let rotateWithSameFacing (x,y,z) = [(x,y,z), (-y,x,z), (-x,-y,z), (y,-x,z)]
+      -- then changing the facing direction: look [same], behind, up, down, left, right
+      turnAroundOrNot (x,y,z) = [(x,y,z), (-x,y,-z)]
+      lookSameUpOrRight (x,y,z) = [(x,y,z), (x,z,-y), (-z,y,x)]
+  in [dir | axis <- lookSameUpOrRight p, r <- rotateWithSameFacing axis, dir <- turnAroundOrNot r]
+
+findOverlapParsing :: [Char] -> [Char] -> [(Int, Int, Int)]
+findOverlapParsing scanner0 scanner1 =
+  let beacons0 = map parseBeacon $ lines scanner0
+      beacons1 = map parseBeacon $ lines scanner1
+      (overlap, _) = findOverlap beacons0 beacons1
+  in overlap
--- test/Day19Spec.hs
+++ test/Day19Spec.hs
@@ -3,10 +3,32 @@ module Day19Spec where
 
 import Test.Hspec
 
+import Data.List (group, sort)
+
 import Day19
 
 spec :: Spec
 spec = do
+  describe "rotation logic" $ do
+    it "creates proper number of distinct rotations" $ do
+      length (group $ sort $ rotationsOld (5,1,20)) `shouldBe` 24
+
+  describe "overlap logic" $ do
+    it "can handle given 3d example" $ do
+      [scanner0, scanner1] <- traverse (\n -> readFile ("day19-example/" ++ show n)) [0,1]
+      findOverlapParsing scanner0 scanner1 `shouldMatchList` [(-618,-824,-621),
+                                                                       (-537,-823,-458),
+                                                                       (-447,-329,318 ),
+                                                                       (404,-588,-901 ),
+                                                                       (544,-627,-890 ),
+                                                                       (528,-643,409  ),
+                                                                       (-661,-816,-575),
+                                                                       (390,-675,-793 ),
+                                                                       (423,-701,434  ),
+                                                                       (-345,-311,381 ),
+                                                                       (459,-707,401  ),
+                                                                       (-485,-357,347 )]
+
   describe "Part 1" $ do
     it "can handle given example" $ do
       inputs <- traverse (\n -> readFile ("day19-example/" ++ show n)) [0..4]
